43872eed5ec5ce2f8daa1733b138523e51d0f91a
RamRamesh1431 <cherukuramesh40@gmail.com>
2022-10-28 19:19:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import LFuncCompo from "./Assignment1/LFuncCompo";
+import LClassCompo from "./Assignment1/LClassCompo";
+import "./Assignment1/myStyle1.css";
+
+class App extends React.Component {
+  state = {
+    showClass: false,
+    showFunc: false,
+  };
+  render() {
+    return (
+      <div>
+        <h1 className="minus">Styling using Functional and Class Component</h1>
+        <button
+          onClick={() => this.setState({ showFunc: !this.state.showFunc })}
+          className="first"
+        >
+          To see styling in functional component
+        </button>
+        <button
+          onClick={() => this.setState({ showClass: !this.state.showClass })}
+          className="second"
+        >
+          To see styling in class component
+        </button>
+        {this.state.showFunc && <LFuncCompo />}
+        {this.state.showClass && <LClassCompo />}
+      </div>
+    );
+  }
+}
+
+export default App;
